5a00f46cc61dcbf77564860d4acfbbae60e80456
Alessia-C <caporasoalessia1611@gmail.com>
2024-07-08 20:58:43 +0200

    fix card

--- src/components/CardUser/CardUser.jsx
+++ src/components/CardUser/CardUser.jsx
@@ -3,6 +3,7 @@ import Card from "../UI/Card/Card";
 import { Avatar, Box, Button, Chip, Typography } from "@mui/material";
 import classes from "./CardUser.module.css";
 import { Link, useNavigate } from "react-router-dom";
+import { height } from "@mui/system";
 
 const CardUser = ({ card, cardElement }) => {
   const navigate = useNavigate();
@@ -23,29 +24,31 @@ const CardUser = ({ card, cardElement }) => {
   // };
 
   return (
-    <Card>
-      <Box className={classes.wrapCardContent}>
-        <Box className={classes.introCard}>
-          <Typography variant="h5">{card.full_name}</Typography>
-          <Chip
-            label={`${card.position}, ${card.department}`}
-            sx={{ fontWeight: "800", fontSize: "16px" }}
-          />
-          {/* {switchComponent(element, card)} */}
-        </Box>
-        <Box sx={{ width: "100%" }} className={classes.content}>
-          <Box className={classes.wrapCta}>
-            <Button
-              variant="contained"
-              type="button"
-              onClick={() => navigate(`detailemployee/${card.id}`)}
-            >
-              Dettaglio
-            </Button>
+    <Box>
+      <Card>
+        <Box className={classes.wrapCardContent}>
+          <Box className={classes.introCard}>
+            <Typography variant="h5">{card.full_name}</Typography>
+            <Chip
+              label={`${card.position}, ${card.department}`}
+              sx={{ fontWeight: "800", fontSize: "16px" }}
+            />
+            {/* {switchComponent(element, card)} */}
+          </Box>
+          <Box sx={{ width: "100%" }} className={classes.content}>
+            <Box className={classes.wrapCta}>
+              <Button
+                variant="contained"
+                type="button"
+                onClick={() => navigate(`detailemployee/${card.id}`)}
+              >
+                Dettaglio
+              </Button>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Card>
+      </Card>
+    </Box>
   );
 };
 
